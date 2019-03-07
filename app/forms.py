6c3101ca7c93fef310b405424474e066b93870cd
Lindsay Pink <lindsay.pink@mymona.uwi.edu>
2019-03-07 23:12:22 +0000

    create user class

---
+++ app/forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import StringField, TextAreaField
+from wtforms.validators import InputRequired, Email
+
+
+class User(FlaskForm):
+    firstname = StringField('First Name', validators=[InputRequired()])
+    lastname = StringField('Last Name', validators=[InputRequired()])
+    gender = StringField('Gender', validators=[InputRequired()])
+    email = StringField('Email', validators=[InputRequired(), Email(message="That's not an email address")])
+    location = StringField('Location', validators=[InputRequired()])
+    bio = TextAreaField('Biography', validators=[InputRequired()])
+    photo = FileField('Photo', validators=[FileRequired(), FileAllowed(['jpg', 'jpeg', 'png'], message="That's not a photo")])
