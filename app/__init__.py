79c6211109381103776069682b1d93b8ee06a90e
Lindsay Pink <lindsay.pink@mymona.uwi.edu>
2019-03-16 21:12:24 +0000

    1st migration and app init files

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "Ax_bd3ktna_n3hgj22r9x4"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://lab5:lab5@localhost/lab5"
+app.config['UPLOAD_FOLDER'] = "./app/static/uploads"
+app.config['SECRET_KEY'] = "Cz_bd3krst_n3hgj94r9x4"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1:project1@localhost/users"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
